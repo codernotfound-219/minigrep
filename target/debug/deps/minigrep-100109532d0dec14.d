cf38a32e98051780f88e17e7671e22d02ba7edc8
codernotfound-219 <aswatt219@icloud.com>
2025-05-10 15:18:43 +0400

    feat: move to lib

---
+++ target/debug/deps/minigrep-100109532d0dec14.d
@@ -0,0 +1,5 @@
+/Users/aswatt/Development/Rust/projects/minigrep/target/debug/deps/libminigrep-100109532d0dec14.rmeta: src/main.rs
+
+/Users/aswatt/Development/Rust/projects/minigrep/target/debug/deps/minigrep-100109532d0dec14.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/minigrep-cc2cf99d8e991baf.d
@@ -0,0 +1,5 @@
+/Users/aswatt/Development/Rust/projects/minigrep/target/debug/deps/libminigrep-cc2cf99d8e991baf.rmeta: src/lib.rs
+
+/Users/aswatt/Development/Rust/projects/minigrep/target/debug/deps/minigrep-cc2cf99d8e991baf.d: src/lib.rs
+
+src/lib.rs:
